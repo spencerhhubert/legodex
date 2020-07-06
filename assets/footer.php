1e3fae3e3aeb962a9d6b2e203decde60ba6e23a2
spencerhhubert <33559710+spencerhhubert@users.noreply.github.com>
2020-07-06 01:09:31 -0700

    Update footer.php

--- assets/footer.php
+++ assets/footer.php
@@ -14,6 +14,8 @@
     </section>
     <section class="container">
         <p style="margin-top: 0; white-space: normal; text-align: center;">LEGO® is a trademark of the LEGO Group of companies which does not sponsor, authorize, or endorse this site.</p>
+    </section>
+    <section class="container">
         <p style="margin-top: 0; white-space: normal; text-align: center;">By using The LEGO YouTuber Index, you are agreeing to the <a href="https://www.youtube.com/t/terms" target="_blank">YouTube Terms of Service</a>.</p>
     </section>
 </footer>
