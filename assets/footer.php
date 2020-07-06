768c791a28dadbe9ec7a80f65610aa0fc64297ed
spencerhhubert <spencerhhubert@gmail.com>
2020-07-06 01:34:28 -0700

    tos and privary

--- assets/footer.php
+++ assets/footer.php
@@ -1,7 +1,7 @@
 <footer class="footer">
     <section class="container" style="align-items: center;">
         <div class="item">
-                <div class="container">
+            <div class="container">
                 <p>By </p>
                 <a href="https://youtube.com/legospencer11" target="_blank"><p>Spencer Hubert</p></a>
             </div>
@@ -12,10 +12,32 @@
             </a>
         </div>
     </section>
-    <section class="container">
-        <p style="margin-top: 0; white-space: normal; text-align: center;">LEGO® is a trademark of the LEGO Group of companies which does not sponsor, authorize, or endorse this site.</p>
+
+    <section class="container" style="align-items: center;">
+        <div class="item">
+            <div class="container">
+                <p>LEGO® is a trademark of the LEGO Group of companies which does not sponsor, authorize, or endorse this site.</p>
+            </div>
+        </div>
     </section>
-    <section class="container">
-        <p style="margin-top: 10px; white-space: normal; text-align: center;">By using The LEGO YouTuber Index, you are agreeing to the </p><a href="https://www.youtube.com/t/terms" target="_blank"><p style="margin-top: 10px;">YouTube Terms of Service.</p></a>
+
+    <section class="container" style="align-items: center;">
+        <div class="item">
+            <div class="container">
+                <p>By using The LEGO YouTuber Index, you are agreeing to the </p>
+                <a href="https://www.youtube.com/t/terms" target="_blank"><p>YouTube Terms of Service</p></a>
+                <p>.</p>
+            </div>
+        </div>
+    </section>
+
+    <section class="container" style="align-items: center;">
+        <div class="item">
+            <div class="container">
+                <p>You can also review </p>
+                <a href="https://www.youtube.com/t/terms" target="_blank"><p>Google's Privacy Policy</p></a>
+                <p>, which The LEGO YouTuber Index adheres to.</p>
+            </div>
+        </div>
     </section>
 </footer>
