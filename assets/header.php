751191c553e24116d2818ad65334d95b1f2e47b2
spencerhhubert <spencerhhubert@gmail.com>
2020-08-12 21:29:48 -0700

    updated tracking

--- assets/header.php
+++ assets/header.php
@@ -17,13 +17,13 @@
   <script src="https://kit.fontawesome.com/21a7b3848c.js" crossorigin="anonymous"></script>
 
   <!-- Global site tag (gtag.js) - Google Analytics -->
-  <script async src="https://www.googletagmanager.com/gtag/js?id=UA-129087319-2"></script>
+  <script async src="https://www.googletagmanager.com/gtag/js?id=UA-129292476-4"></script>
   <script>
     window.dataLayer = window.dataLayer || [];
     function gtag(){dataLayer.push(arguments);}
     gtag('js', new Date());
 
-    gtag('config', 'UA-129087319-2');
+    gtag('config', 'UA-129292476-4');
   </script>
 
   <script
