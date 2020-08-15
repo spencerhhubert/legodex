e913ce6dd0988f2077a79136811c7ddc6f8ad7cb
spencerhhubert <spencerhhubert@gmail.com>
2020-08-15 13:40:19 -0700

    tracking comes first

--- feelinglucky.php
+++ feelinglucky.php
@@ -1,14 +1,3 @@
-<?php
-include_once 'assets/dbconn.php';
-include 'assets/functions.php';
-
-$luckyChannelId = mysqli_fetch_assoc(mysqli_query($conn, "SELECT id FROM channels ORDER BY RAND() LIMIT 1"));
-$luckyChannelLink = makeChannelLink($luckyChannelId['id']);
-
-header("Location: " . $luckyChannelLink);
-
-?>
-
 <!-- Global site tag (gtag.js) - Google Analytics -->
 <script async src="https://www.googletagmanager.com/gtag/js?id=UA-129292476-4"></script>
 <script>
@@ -18,3 +7,13 @@ gtag('js', new Date());
 
 gtag('config', 'UA-129292476-4');
 </script>
+
+<?php
+include_once 'assets/dbconn.php';
+include 'assets/functions.php';
+
+$luckyChannelId = mysqli_fetch_assoc(mysqli_query($conn, "SELECT id FROM channels ORDER BY RAND() LIMIT 1"));
+$luckyChannelLink = makeChannelLink($luckyChannelId['id']);
+
+header("Location: " . $luckyChannelLink);
+?>
