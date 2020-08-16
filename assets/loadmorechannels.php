29b881d5559c7f63c0f558c00435d9ecd27f6c01
spencerhhubert <spencerhhubert@gmail.com>
2020-08-16 15:23:10 -0700

    minor fix in displaying order

--- assets/loadmorechannels.php
+++ assets/loadmorechannels.php
@@ -2,7 +2,7 @@
     include 'dbconn.php';
     include 'functions.php';
     $channelNewCount = $_POST['channelNewCount'];
-    $result = mysqli_query($conn, "SELECT * FROM channels ORDER BY vidDate DESC LIMIT $channelNewCount");
+    $result = mysqli_query($conn, "SELECT * FROM channels ORDER BY isLive DESC, vidDate DESC LIMIT $channelNewCount");
     $resultCheck = mysqli_num_rows($result);
 
     include 'loadchannels.php'
